da793a31a0bc2c93d0a39c45217b5b0469bbdb0e
Rafael do Prado <rafa40026@gmail.com>
2025-03-20 06:04:02 -0300

    Add files via upload

---
+++ exercicios-Javascript/script.js
@@ -0,0 +1,45 @@
+// var dado = "Rafael" /* string */
+// alert(dado)
+
+// var dado = 123 /* number */
+// alert (dado)
+
+// var dado = true /* Boolean - ture e false */
+// alert (dado)
+
+// var dado = /* Undefined */
+// // alert (dado)
+
+
+
+
+/* OPERADORES ARITMÉTICOS */
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ 
+ADIÇÃO 9 + 3 = 12
+SUBTRAÇÃO 9 - 3 = 6
+DIVISÃO 9 / 3 = 3
+ MULTIPLICAÇÃO 9 * 3 = 27
+ POTENCIA  9 ** 3 = 9 * 9 * 9
+ RESTO DA DIVISÃO  10 % 3 = 0   0, 1 E 2
+*/
+
+var numero1 = 9
+var numero2 = 3
+
+var soma = numero1 + numero2
+
+
+
